ca5668c312259d1af9f68881c9bfdeeda7cd1821
Jamison85 <jamisonr85@gmail.com>
2026-09-18 09:32:20 -0500

    Force refresh for reset update fix

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const VERSION = 'findtrail-v2.4.4-approved-reset-art-2026-09-18'
+const VERSION = 'findtrail-v2.4.5-update-everywhere-2026-09-18'
 const STATIC_CACHE = `${VERSION}-static`
 const RUNTIME_CACHE = `${VERSION}-runtime`
 const BASE_PATH = new URL(self.registration.scope).pathname.replace(/\/$/, '')
