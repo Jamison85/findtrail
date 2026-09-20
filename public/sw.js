266c771a3b5be3b1911aeb8d0c517107a09697e7
Jamison85 <jamisonr85@gmail.com>
2026-09-20 08:08:04 -0500

    Refresh cache for side breathing cue

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const VERSION = 'findtrail-v2.4.13-auto-refresh-2026-09-20'
+const VERSION = 'findtrail-v2.4.14-side-breath-cue-2026-09-20'
 const STATIC_CACHE = `${VERSION}-static`
 const RUNTIME_CACHE = `${VERSION}-runtime`
 const BASE_PATH = new URL(self.registration.scope).pathname.replace(/\/$/, '')
