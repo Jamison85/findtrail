30af8eed43771cc266387d0eae60f82cf0da5376
Jamison85 <jamisonr85@gmail.com>
2026-09-20 08:12:18 -0500

    Refresh cache for peaceful cue

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const VERSION = 'findtrail-v2.4.14-side-breath-cue-2026-09-20'
+const VERSION = 'findtrail-v2.4.15-peaceful-breath-cue-2026-09-20'
 const STATIC_CACHE = `${VERSION}-static`
 const RUNTIME_CACHE = `${VERSION}-runtime`
 const BASE_PATH = new URL(self.registration.scope).pathname.replace(/\/$/, '')
